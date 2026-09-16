f4addda7702deb1510018815aef9486e26f3b931
Hadden Smith <HaddenASmith@gmail.com>
2026-09-16 12:44:54 -0600

    Added index.html and deplyFiles.sh from simon-html repository as a base to start

---
+++ deployFiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SSH="/c/Windows/System32/OpenSSH/ssh.exe"
+SCP="/c/Windows/System32/OpenSSH/scp.exe"
+#./deployFiles.sh -h haddensmith.com -s simon Code to deplot in production
+
+while getopts h:s: flag
+do
+    case "${flag}" in
+        h) hostname=${OPTARG};;
+        s) service=${OPTARG};;
+    esac
+done
+
+if [[ -z "$hostname" || -z "$service" ]]; then
+    printf "\nMissing required parameter.\n"
+    printf "  syntax: deployFiles.sh -h <hostname> -s <service>\n\n"
+    exit 1
+fi
+
+printf "\n----> Deploying files for $service to $hostname\n"
+
+# Step 1
+printf "\n----> Clear out the previous distribution on the target.\n"
+"$SSH" ubuntu@$hostname << ENDSSH
+rm -rf services/${service}/public
+mkdir -p services/${service}/public
+ENDSSH
+
+# Step 2
+printf "\n----> Copy the distribution package to the target.\n"
+"$SCP" -r * ubuntu@$hostname:services/$service/public
