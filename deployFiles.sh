86d0c173464c09bbd1f0471a04c1ddd309f1ab54
Hadden Smith <HaddenASmith@gmail.com>
2026-09-23 22:42:30 -0600

    Updated deployFiles.sh to match the simon-css version

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Use Windows OpenSSH so authentication is handled by the Bitwarden SSH Agent
-SSH="/c/Windows/System32/OpenSSH/ssh.exe"
-SCP="/c/Windows/System32/OpenSSH/scp.exe"
-
-while getopts h:s: flag
+while getopts k:h:s: flag
 do
     case "${flag}" in
+        k) key=${OPTARG};;
         h) hostname=${OPTARG};;
         s) service=${OPTARG};;
     esac
 done
 
-if [[ -z "$hostname" || -z "$service" ]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname using Bitwarden SSH Agent\n"
+printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-"$SSH" ubuntu@"$hostname" << ENDSSH
+ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-"$SCP" -r * ubuntu@"$hostname":services/"$service"/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
