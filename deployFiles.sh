24d2459b28f9488833a8ef22d30b758261cb664b
Hadden Smith <HaddenASmith@gmail.com>
2026-09-23 22:39:14 -0600

    Updated deployFiles.sh to match the simon-css version

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+# Use Windows OpenSSH so authentication is handled by the Bitwarden SSH Agent
 SSH="/c/Windows/System32/OpenSSH/ssh.exe"
 SCP="/c/Windows/System32/OpenSSH/scp.exe"
-# ./deployFiles.sh -h haddensmith.com -s startup
-# Code to deploy files to a remote server using SSH and SCP
 
 while getopts h:s: flag
 do
@@ -19,15 +18,15 @@ if [[ -z "$hostname" || -z "$service" ]]; then
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname\n"
+printf "\n----> Deploying files for $service to $hostname using Bitwarden SSH Agent\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-"$SSH" ubuntu@$hostname << ENDSSH
+"$SSH" ubuntu@"$hostname" << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-"$SCP" -r * ubuntu@$hostname:services/$service/public
+"$SCP" -r * ubuntu@"$hostname":services/"$service"/public
